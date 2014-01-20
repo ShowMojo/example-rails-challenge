f5524ed6aad8aea79a84383739a3f6907ae12007
Igor Bozato <igor.hrk@gmail.com>
2014-01-20 21:33:02 -0200

    Charges view and correted tests

--- source/spec/features/charges_spec.rb
+++ source/spec/features/charges_spec.rb
@@ -9,21 +9,21 @@ feature "Charges" do
     expect(page).to have_content "Successful Charges"
   end
 
-  it "lists 10 successful chages" do
+  it "lists 10 successful charges" do
     visit "/charges"
 
-    expect(find("#successful tr").count).to eq(11)
+    expect(all("#successful tr").count).to eq(11)
   end
 
-  it "lists 5 failed chages" do
+  it "lists 5 failed charges" do
     visit "/charges"
 
-    expect(find("#failed tr").count).to eq(6)
+    expect(all("#failed tr").count).to eq(6)
   end
 
-  it "lists failed chages in disputed list" do
+  it "lists 5 disputed charges" do
     visit "/charges"
 
-    pending
+    expect(all("#failed tr").count).to eq(6)
   end
 end
