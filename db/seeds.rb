c5ab1204d0eed6c72638501de8042f35469c7342
Fernando Serapio <fhernandosc@gmail.com>
2015-01-02 14:59:26 -0600

    add seeds for customers and charges

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,109 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+customer_1 = Customer.create first_name: 'Johny', last_name: 'Flow'
+customer_2 = Customer.create first_name: 'Raj', last_name: 'Jamnis'
+customer_3 = Customer.create first_name: 'Andrew', last_name: 'Chung'
+customer_4 = Customer.create first_name: 'Mike', last_name: 'Smith'
+
+puts 'Seed successful charges ...'
+# Customer 1
+(1..5).each do
+  Charge.create(
+    created: Time.now.to_f * 1000,
+    paid: true,
+    amount: (1000.0 - 5.0) * rand() + 5,
+    currency: 'usd',
+    refunded: false,
+    customer: customer_1
+  )
+end
+
+# Customer 2
+(1..3).each do
+  Charge.create(
+    created: Time.now.to_f * 1000,
+    paid: true,
+    amount: (1000.0 - 5.0) * rand() + 5,
+    currency: 'usd',
+    refunded: false,
+    customer: customer_2
+  )
+end
+
+# Customer 3
+Charge.create(
+  created: Time.now.to_f * 1000,
+  paid: true,
+  amount: (1000.0 - 5.0) * rand() + 5,
+  currency: 'usd',
+  refunded: false,
+  customer: customer_3
+)
+
+# Customer 4
+Charge.create(
+  created: Time.now.to_f * 1000,
+  paid: true,
+  amount: (1000.0 - 5.0) * rand() + 5,
+  currency: 'usd',
+  refunded: false,
+  customer: customer_4
+)
+
+puts 'Done!'
+
+puts 'Seed failed charges ...'
+
+# Customer 3
+(1..3).each do
+  Charge.create(
+  created: Time.now.to_f * 1000,
+  paid: false,
+  amount: (1000.0 - 5.0) * rand() + 5,
+  currency: 'usd',
+  refunded: false,
+  customer: customer_3
+  )
+end
+
+# Customer 4
+(1..2).each do
+  Charge.create(
+  created: Time.now.to_f * 1000,
+  paid: false,
+  amount: (1000.0 - 5.0) * rand() + 5,
+  currency: 'usd',
+  refunded: false,
+  customer: customer_4
+  )
+end
+
+puts 'Done!'
+
+puts 'Seed disputed chages'
+
+# Customer 1
+(1..3).each do
+  Charge.create(
+  created: Time.now.to_f * 1000,
+  paid: true,
+  amount: (1000.0 - 5.0) * rand() + 5,
+  currency: 'usd',
+  refunded: true,
+  customer: customer_1
+  )
+end
+
+# Customer 2
+(1..2).each do
+  Charge.create(
+  created: Time.now.to_f * 1000,
+  paid: true,
+  amount: (1000.0 - 5.0) * rand() + 5,
+  currency: 'usd',
+  refunded: true,
+  customer: customer_2
+  )
+end
+
+puts 'Done!'
+
+puts 'Finish'
