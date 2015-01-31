441a8941222161085adbecbc55c43049fc9b78b4
Serghei <agilestep@yahoo.com>
2015-01-31 17:07:20 +0200

    Adding tests.

--- app_challenge/spec/views/card_charges/index.html.haml_spec.rb
+++ app_challenge/spec/views/card_charges/index.html.haml_spec.rb
@@ -1,5 +1,32 @@
 require 'spec_helper'
 
-describe "card_charges/index.html.haml" do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe "card_charges/index.html.haml", :type => :view do
+  before do
+    @failed,@successful,@disputed = CardCharge.failed,CardCharge.successful, CardCharge.disputed
+    render
+  end
+
+
+  it 'it render a partial and show 3 lists' do
+    render
+    expect(rendered).to include("tab_success")
+    expect(rendered).to include("tab_failed")
+    expect(rendered).to include("tab_disputed")
+  end
+
+
+  it 'list "successful" should contain 10 elements ' do
+    expect(rendered).to include("Successful Count:10")
+  end
+
+
+  it 'list "Failed" should contain 5 elements ' do
+    expect(rendered).to include("Failed Count:5")
+  end
+
+
+  it 'list "Disputed" should contain 5 elements ' do
+    expect(rendered).to include("Disputed Count:5")
+  end
+
 end
