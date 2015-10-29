c53d45666ae67722b5051b41bcaf483dbfab48a0
Vik Lits <viklits@yahoo.com>
2015-10-29 15:21:03 +0200

    add seeds

--- source/db/seeds.rb
+++ source/db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+amounts   = (1..10).to_a
+currency  = 'usd'
+
+[
+  { first_name: 'Johny',  last_name: 'Flow',    ok: 5, failed: 0, disputed: 3 },
+  { first_name: 'Raj',    last_name: 'Jamnis',  ok: 3, failed: 0, disputed: 2 },
+  { first_name: 'Andrew', last_name: 'Chung',   ok: 1, failed: 3, disputed: 0 },
+  { first_name: 'Mike',   last_name: 'Smith',   ok: 1, failed: 2, disputed: 0 }
+].each do |options|
+  customer = Customer.create(
+    first_name: options[:first_name],
+    last_name:  options[:last_name]
+  )
+
+  options[:ok].times do
+    customer.charges.create(
+      paid:     true,
+      amount:   amounts.sample,
+      currency: currency,
+      refunded: false,
+    )
+  end
+
+  options[:failed].times do
+    customer.charges.create(
+      paid:     false,
+      amount:   amounts.sample,
+      currency: currency,
+      refunded: false,
+    )
+  end
+
+  options[:disputed].times do
+    customer.charges.create(
+      paid:     true,
+      amount:   amounts.sample,
+      currency: currency,
+      refunded: true,
+    )
+  end
+end
