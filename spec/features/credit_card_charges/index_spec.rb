cf590166298ce51cf05dba1849c73854341bbefd
Spiceee <spiceee@gmail.com>
2015-11-11 14:10:12 -0200

    integration tests

--- spec/features/credit_card_charges/index_spec.rb
+++ spec/features/credit_card_charges/index_spec.rb
@@ -13,13 +13,48 @@ feature 'Credit Card Charges' do
     expect(page).to have_selector(:css, "table[class='failed table']")
   end
 
+  # Scenario: Successful Charges on index page
+  #   Given I am signed in
+  #   When I visit the charges index page
+  #   Then I see successful charges
   scenario 'user sees successful charges' do
     visit credit_card_charges_path
     expect(page).to have_selector(:css, "table[class='successful table']")
   end
 
+  # Scenario: Disputed Charges on index page
+  #   Given I am signed in
+  #   When I visit the charges index page
+  #   Then I see disputed charges
   scenario 'user sees disputed charges' do
     visit credit_card_charges_path
     expect(page).to have_selector(:css, "table[class='disputed table']")
+  end
+
+  # Scenario: Successful Charges on index page
+  #   Given I am signed in
+  #   When I visit the charges index page
+  #   Then I see 10 successful charges
+  scenario 'user sees 10 successful charges' do
+    visit credit_card_charges_path
+    expect(page).to have_selector(:css, "table[class='successful table'] tr[class=charge]", count: 10)
+  end
+
+  # Scenario: Failed Charges on index page
+  #   Given I am signed in
+  #   When I visit the charges index page
+  #   Then I see 5 failed charges
+  scenario 'user sees 5 failed charges' do
+    visit credit_card_charges_path
+    expect(page).to have_selector(:css, "table[class='failed table'] tr[class=charge]", count: 5)
+  end
+
+  # Scenario: Disputed Charges on index page
+  #   Given I am signed in
+  #   When I visit the charges index page
+  #   Then I see 5 disputed charges
+  scenario 'user sees 5 disputed charges' do
+    visit credit_card_charges_path
+    expect(page).to have_selector(:css, "table[class='disputed table'] tr[class=charge]", count: 5)
   end
 end
