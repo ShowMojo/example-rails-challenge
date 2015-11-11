a74daef17acbad5a2d7cdc3600cbde2cf517d2fa
Spiceee <spiceee@gmail.com>
2015-11-11 13:18:43 -0200

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :customers
   resources :credit_card_charges
 
+  root 'credit_card_charges#index'
+  get 'charges/' => 'credit_card_charges#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
