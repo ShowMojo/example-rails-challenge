b79c27370a8a4cd235937c609ffb351127c1171d
Ramil <ramil.gilmanov@flatstack.com>
2015-11-14 12:27:16 +0300

    Seed data

--- source/db/seeds.rb
+++ source/db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Customers
+customer1 = Customer.create(first_name: "Johny", last_name: "Flow")
+customer2 = Customer.create(first_name: "Raj", last_name: "Jamnis")
+customer3 = Customer.create(first_name: "Andrew", last_name: "Chung")
+customer4 = Customer.create(first_name: "Mike", last_name: "Smith")
+
+# Successful transactions
+5.times { Charge.create(customer: customer1, amount: rand(10_000..100_0000), currency: "USD", status: :success) }
+3.times { Charge.create(customer: customer2, amount: rand(10_000..100_0000), currency: "USD", status: :success) }
+Charge.create(customer: customer3, amount: rand(10_000..100_0000), currency: "USD", status: :success)
+Charge.create(customer: customer4, amount: rand(10_000..100_0000), currency: "USD", status: :success)
+
+# Failed transactions
+3.times { Charge.create(customer: customer3, amount: rand(10_000..100_0000), currency: "USD", status: :failed) }
+2.times { Charge.create(customer: customer4, amount: rand(10_000..100_0000), currency: "USD", status: :failed) }
+
+# Disputed transactions
+3.times { Charge.create(customer: customer1, amount: rand(10_000..100_0000), currency: "USD", status: :disputed) }
+2.times { Charge.create(customer: customer2, amount: rand(10_000..100_0000), currency: "USD", status: :disputed) }
