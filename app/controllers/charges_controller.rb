6562f1b7b0c72837be5e7dffb3766a92848f37e9
wangbin <wangbin_62@yahoo.com>
2016-06-08 01:39:35 +0800

    dry controller

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,7 +1,5 @@
 class ChargesController < ApplicationController
   def index
-    @failed_charges = Charge.where(paid: false)
-    @disputed_charges = Charge.where(paid: true, refunded: true)
-    @successful_charges = Charge.where(paid: true, refunded: false)
+    @charges = Charge.all
   end
 end
