0f1b52ebcccac00ad72cc65f35b0cd806ebd6529
wangbin <wangbin_62@yahoo.com>
2016-06-07 20:47:34 +0800

    add customer model, charge model , db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+customer_1 = Customer.find_or_create_by(fname: 'Johny', lname: 'Flow')
+customer_2 = Customer.find_or_create_by(fname: 'Raj', lname: 'Jamnis')
+customer_3 = Customer.find_or_create_by(fname: 'Andrew', lname: 'Chung')
+customer_4 = Customer.find_or_create_by(fname: 'Mike', lname: 'Smith')
+
+5.times {Charge.create(paid: true, refunded: false, amount: rand(0..10000), customer: customer_1)}
+3.times {Charge.create(paid: true, refunded: false, amount: rand(0..10000), customer: customer_2)}
+1.times {Charge.create(paid: true, refunded: false, amount: rand(0..10000), customer: customer_3)}
+1.times {Charge.create(paid: true, refunded: false, amount: rand(0..10000), customer: customer_4)}
+
+3.times {Charge.create(paid: false, refunded: false, amount: rand(0..10000), customer: customer_3)}
+2.times {Charge.create(paid: false, refunded: false, amount: rand(0..10000), customer: customer_4)}
+
+3.times {Charge.create(paid: true, refunded: true, amount: rand(0..10000), customer: customer_1)}
+2.times {Charge.create(paid: true, refunded: true, amount: rand(0..10000), customer: customer_2)}
