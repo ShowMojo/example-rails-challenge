0e46be68e59414b0dab3583dcf7127e0296e768b
Admin <thomasdev99@yandex.com>
2016-06-07 23:48:08 +0800

    added rspec test...

--- spec/views/creaditcharges/index.html.erb_spec.rb
+++ spec/views/creaditcharges/index.html.erb_spec.rb
@@ -1,28 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe "creaditcharges/index", type: :view do
-  before(:each) do
-    assign(:creaditcharges, [
-      Creaditcharge.create!(
-        :paid => false,
-        :amount => 1,
-        :currency => "Currency",
-        :refunded => ""
-      ),
-      Creaditcharge.create!(
-        :paid => false,
-        :amount => 1,
-        :currency => "Currency",
-        :refunded => ""
-      )
-    ])
-  end
+#   before(:each) do
+#     assign(:creaditcharges, [
+#       Creaditcharge.create!(
+#         :paid => false,
+#         :amount => 1,
+#         :currency => "Currency",
+#         :refunded => ""
+#       ),
+#       Creaditcharge.create!(
+#         :paid => false,
+#         :amount => 1,
+#         :currency => "Currency",
+#         :refunded => ""
+#       )
+#     ])
+#   end
 
   it "renders a list of creaditcharges" do
     render
-    assert_select "tr>td", :text => false.to_s, :count => 2
-    assert_select "tr>td", :text => 1.to_s, :count => 2
-    assert_select "tr>td", :text => "Currency".to_s, :count => 2
-    assert_select "tr>td", :text => "".to_s, :count => 2
+    assert_select "body>h1", :count => 3
+    assert_select "div.failed", :count => Creaditcharge.failed.count
   end
 end
