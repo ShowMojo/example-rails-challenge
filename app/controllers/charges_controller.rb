c816b8a8ccb87b482730814cc04f84529a4ae8c6
wangbin <wangbin_62@yahoo.com>
2016-06-08 01:24:48 +0800

    fixed disputed condition

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,7 +1,7 @@
 class ChargesController < ApplicationController
   def index
     @failed_charges = Charge.where(paid: false)
-    @disputed_charges = Charge.where(refunded: true)
+    @disputed_charges = Charge.where(paid: true, refunded: true)
     @successful_charges = Charge.where(paid: true, refunded: false)
   end
 end
