9548f4ee37e9351a5e2f6e06fe36fa4974127c67
Admin <thomasdev99@yandex.com>
2016-06-07 22:20:54 +0800

    final test ...

---
+++ app/models/creaditcharge.rb
@@ -0,0 +1,9 @@
+class Creaditcharge < ActiveRecord::Base
+
+  belongs_to :customer
+
+  scope :failed, -> { where(:paid => false) }
+  scope :disputed, -> { where(:paid => true, :refunded => true) }
+  scope :successful, -> { where(:paid => true, :refunded => false) }
+
+end
