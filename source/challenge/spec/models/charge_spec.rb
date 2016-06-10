b7d63c5a720d2d2cba5ea1c6e14b6fdbb2256bfd
Augusto Zangrandi <augusto.zangrandi@gmail.com>
2016-06-10 17:49:26 -0300

    Customer test on the right file

--- source/challenge/spec/models/charge_spec.rb
+++ source/challenge/spec/models/charge_spec.rb
@@ -2,10 +2,4 @@ require 'rails_helper'
 
 RSpec.describe Charge, type: :model do
   
-  describe "#full_name" do
-  	it "should merge first name last name" do
-  		customer = Customer.new(first_name: "John", last_name: "Doe")
-  		expect(customer.full_name).to eql("John Doe")
-  	end
-  end
 end
--- source/challenge/spec/models/customer_spec.rb
+++ source/challenge/spec/models/customer_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#full_name" do
+  	it "should merge first name last name" do
+  		customer = Customer.new(first_name: "John", last_name: "Doe")
+  		expect(customer.full_name).to eql("John Doe")
+  	end
+  end
 end
