443f10187a59154018f5378dcc2e96559d78cc3d
Augusto Zangrandi <augusto.zangrandi@gmail.com>
2016-06-10 16:55:49 -0300

    Create seeds

--- source/challenge/db/seeds.rb
+++ source/challenge/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+customer_1 = Customer.create(first_name: "Johny", last_name: "Flow")
+customer_2 = Customer.create(first_name: "Raj", last_name: "Jamnis")
+customer_3 = Customer.create(first_name: "Andrew", last_name: "Chung")
+customer_4 = Customer.create(first_name: "Mike", last_name: "Smith")
+
+5.times { customer_1.charges.create(paid: true, amount: rand(10000), currency: "usd", refunded: false) }
+3.times { customer_2.charges.create(paid: true, amount: rand(10000), currency: "usd", refunded: false) }
+customer_3.charges.create(paid: true, amount: rand(10000), currency: "usd", refunded: false)
+customer_4.charges.create(paid: true, amount: rand(10000), currency: "usd", refunded: false)
+3.times { customer_3.charges.create(paid: false, amount: rand(10000), currency: "usd", refunded: false) }
+2.times { customer_4.charges.create(paid: false, amount: rand(10000), currency: "usd", refunded: false) }
+3.times { customer_1.charges.create(paid: true, amount: rand(10000), currency: "usd", refunded: true) }
+2.times { customer_2.charges.create(paid: true, amount: rand(10000), currency: "usd", refunded: true) }
