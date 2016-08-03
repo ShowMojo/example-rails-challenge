8d4249c1ea295249232f48b0f3233bd162b82f7d
railsdev <santiago.siri@mail.com>
2016-08-03 11:01:12 -0400

    add charge and customer scaffolds

--- source/config/routes.rb
+++ source/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :charges
+  resources :customers
+  get 'customer/first_name'
+
+  get 'customer/last_name'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
