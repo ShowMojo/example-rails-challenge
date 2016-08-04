952e4f6f7c31a1d4ce362e83dc3799742b5389ab
invinc87 <invincible87@126.com>
2016-08-05 02:36:45 +0800

    Show charges list

--- code/db/seeds.rb
+++ code/db/seeds.rb
@@ -10,26 +10,26 @@ customer4 = Customer.create!(first_name: 'Mike', last_name: 'Smith')
 
 # 10 successful charges
 5.times do
-  customer1.charges.successful.create!(amount: rand(0..100000), currency: 'USD')
+  customer1.charges.successful.create!(amount: rand(0..100000), currency: 'usd')
 end
 3.times do
-  customer2.charges.successful.create!(amount: rand(0..100000), currency: 'USD')
+  customer2.charges.successful.create!(amount: rand(0..100000), currency: 'usd')
 end
-customer3.charges.successful.create!(amount: rand(0..100000), currency: 'USD')
-customer4.charges.successful.create!(amount: rand(0..100000), currency: 'USD')
+customer3.charges.successful.create!(amount: rand(0..100000), currency: 'usd')
+customer4.charges.successful.create!(amount: rand(0..100000), currency: 'usd')
 
 # 5 charges that failed
 3.times do
-  customer3.charges.failed.create!(amount: rand(0..100000), currency: 'USD')
+  customer3.charges.failed.create!(amount: rand(0..100000), currency: 'usd')
 end
 2.times do
-  customer4.charges.failed.create!(amount: rand(0..100000), currency: 'USD')
+  customer4.charges.failed.create!(amount: rand(0..100000), currency: 'usd')
 end
 
 # 5 disputed charges
 3.times do
-  customer1.charges.disputed.create!(amount: rand(0..100000), currency: 'USD')
+  customer1.charges.disputed.create!(amount: rand(0..100000), currency: 'usd')
 end
 2.times do
-  customer2.charges.disputed.create!(amount: rand(0..100000), currency: 'USD')
+  customer2.charges.disputed.create!(amount: rand(0..100000), currency: 'usd')
 end
