4c06508650f56af18be4edeaf36ac5024d8b4dcb
Dmitri Kiselevich <kis.dmitri.gen@gmail.com>
2017-01-19 22:36:27 +0200

    add seed

--- source/spec/factories/charges.rb
+++ source/spec/factories/charges.rb
@@ -1,9 +1,23 @@
 FactoryGirl.define do
   factory :charge do
     paid false
-    amount 1
+    amount{rand(10000..100000)}
     currency "MyString"
     refunded false
     customer nil
+
+    trait :succeed do
+      paid true
+      refunded false
+    end
+
+    trait :failed do
+      paid false
+    end
+
+    trait :disputed do
+      paid true
+      refunded true
+    end
   end
 end
