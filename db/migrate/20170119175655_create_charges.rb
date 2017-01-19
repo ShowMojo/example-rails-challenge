2841dc158af0801792a971f24f221e408271fb0e
oozzal <theoozzal@gmail.com>
2017-01-19 23:55:35 +0545

    Add customer and charge models

---
+++ db/migrate/20170119175655_create_charges.rb
@@ -0,0 +1,13 @@
+class CreateCharges < ActiveRecord::Migration[5.0]
+  def change
+    create_table :charges do |t|
+      t.boolean :paid
+      t.integer :amount
+      t.string :currency, default: 'usd'
+      t.boolean :refunded
+      t.references :customer, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
