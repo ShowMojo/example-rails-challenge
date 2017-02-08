16bcedf78c80b76595d9254ad9fbd67d202a18d0
nuvanda <jax.nuvanda@gmail.com>
2017-02-08 17:45:26 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+customer1 = FactoryGirl.create(:customer, first_name: 'Johny', last_name: 'Flow')
+customer2 = FactoryGirl.create(:customer, first_name: 'Raj', last_name: 'Jamnis')
+customer3 = FactoryGirl.create(:customer, first_name: 'Andrew', last_name: 'Chung')
+customer4 = FactoryGirl.create(:customer, first_name: 'Mike', last_name: 'Smith')
+
+# successful
+5.times { FactoryGirl.create(:successfull, customer: customer1) }
+3.times { FactoryGirl.create(:successfull, customer: customer2) }
+FactoryGirl.create(:successfull, customer: customer3)
+FactoryGirl.create(:successfull, customer: customer4)
+
+# failed
+3.times { FactoryGirl.create(:failed, customer: customer3) }
+2.times { FactoryGirl.create(:failed, customer: customer4) }
+
+# disputed
+3.times { FactoryGirl.create(:disputed, customer: customer1) }
+2.times { FactoryGirl.create(:disputed, customer: customer2) }
