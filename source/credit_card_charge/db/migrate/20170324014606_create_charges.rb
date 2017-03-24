71199ada801527dbcd64e18fe0af41b6e8ce9b28
Rolando Alvarado <rorroland@gmail.com>
2017-03-24 10:09:22 +0800

    Setup App and Databases
    - Setup Application
    - Created Database
    - Created models and migration

---
+++ source/credit_card_charge/db/migrate/20170324014606_create_charges.rb
@@ -0,0 +1,13 @@
+class CreateCharges < ActiveRecord::Migration[5.0]
+  def change
+    create_table :charges do |t|
+      t.references :customer
+      
+      t.boolean :paid
+      t.decimal :amount, precision: 7, scale: 2
+      t.string :currency
+      t.boolean :refunded
+      t.timestamps
+    end
+  end
+end
