73f78c61a38f1fe9a4c9e445ac87dc12e07ff1af
Alexey Kartamyshev <sedzen.san@gmail.com>
2018-03-06 08:04:59 +0200

    Scaffold CreditCardCharge and Customer.

---
+++ source/test/system/credit_card_charges_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class CreditCardChargesTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit credit_card_charges_url
+  #
+  #   assert_selector "h1", text: "CreditCardCharge"
+  # end
+end
