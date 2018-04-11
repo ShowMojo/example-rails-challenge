db36b9e29b3e6ecbd938d753d96b3d74a9925e06
Vladimir PC <vladimir_invest@inbox.ru>
2018-04-12 01:58:34 +0300

    Add seed file. Add scopes for card transactions

--- source/credit_card_app/app/models/card_transaction.rb
+++ source/credit_card_app/app/models/card_transaction.rb
@@ -1,3 +1,7 @@
 class CardTransaction < ActiveRecord::Base
   belongs_to :customer
+
+  scope :successful, -> { includes(:customer).where(paid: true, refunded: false) }
+  scope :failed, -> { includes(:customer).where(paid: false, refunded: false) }
+  scope :disputed, -> { includes(:customer).where(paid: true, refunded: true) }
 end
--- source/credit_card_app/db/seeds.rb
+++ source/credit_card_app/db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+customer_1 = Customer.create(first_name: 'Johny', last_name: 'Flow')
+customer_2 = Customer.create(first_name: 'Raj', last_name: 'Jamnis')
+customer_3 = Customer.create(first_name: 'Andrew', last_name: 'Chung')
+customer_4 = Customer.create(first_name: 'Mike', last_name: 'Smith')
+
+transaction_template = -> {{
+    created: Random.new.rand(Time.now.to_i),
+    amount: Random.new.rand(100000),
+    currency: 'usd'
+}}
+
+successful = { paid: true, refunded: false }
+failed = { paid: false, refunded: false }
+disputed = { paid: true, refunded: true }
+
+5.times do
+  customer_1.card_transactions.create(transaction_template.call.merge(successful))
+end
+
+3.times do
+  customer_2.card_transactions.create(transaction_template.call.merge(successful))
+end
+
+customer_3.card_transactions.create(transaction_template.call.merge(successful))
+customer_4.card_transactions.create(transaction_template.call.merge(successful))
+
+3.times do
+  customer_3.card_transactions.create(transaction_template.call.merge(failed))
+  customer_1.card_transactions.create(transaction_template.call.merge(disputed))
+end
+
+2.times do
+  customer_4.card_transactions.create(transaction_template.call.merge(failed))
+  customer_2.card_transactions.create(transaction_template.call.merge(disputed))
+end
