399ae7f26c38df945064c8739db3bbf71fa39ba1
Vladimir PC <vladimir_invest@inbox.ru>
2018-04-12 02:25:11 +0300

    Add controller and view for charges

--- source/credit_card_app/app/models/card_transaction.rb
+++ source/credit_card_app/app/models/card_transaction.rb
@@ -4,4 +4,12 @@ class CardTransaction < ActiveRecord::Base
   scope :successful, -> { includes(:customer).where(paid: true, refunded: false) }
   scope :failed, -> { includes(:customer).where(paid: false, refunded: false) }
   scope :disputed, -> { includes(:customer).where(paid: true, refunded: true) }
+
+  def formatted_amount
+    "#{currency} #{amount/100.0}"
+  end
+
+  def formatted_date
+    Time.at(created).strftime('%c')
+  end
 end
--- source/credit_card_app/config/routes.rb
+++ source/credit_card_app/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/charges', to: 'card_transactions#charges'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
