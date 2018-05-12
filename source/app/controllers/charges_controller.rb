9d51f0fa56d712b130113264b3edada33c01b1d2
Victor Pukman <victor.pukman@gmail.com>
2018-05-13 00:51:17 +0800

    charges view added

--- source/app/controllers/charges_controller.rb
+++ source/app/controllers/charges_controller.rb
@@ -1,4 +1,7 @@
 class ChargesController < ApplicationController
   def index
+    @successful_charges = Charge.successful
+    @failed_charges = Charge.failed
+    @disputed_charges = Charge.disputed
   end
 end
--- source/spec/features/charges_spec.rb
+++ source/spec/features/charges_spec.rb
@@ -1,11 +1,38 @@
 require 'rails_helper'
 
 RSpec.feature "Charges", type: :feature do
-  #before do
+  let!(:successful_charges) { create_list :charge, 5, :successful }
+  let!(:failed_charges) { create_list :charge, 3, :failed }
+  let!(:disputed_charges) { create_list :charge, 10, :disputed }
+
+  before do
+    visit charges_path
+  end
+
+  #it "can be visited" do
   #  visit charges_path
   #end
 
-  it "can be visited" do
-    visit charges_path
+  it "shows failed charges" do
+    within('.failed') do
+      expect(page).to have_text("Failed charges")
+      expect(all('.charge').count).to eq failed_charges.count
+    end
   end
+
+  it "shows disputed charges" do
+    within('.disputed') do
+      expect(page).to have_text("Disputed charges")
+      expect(all('.charge').count).to eq disputed_charges.count
+    end
+  end
+
+  it "shows successful charges" do
+    within('.successful') do
+      expect(page).to have_text("Successful charges")
+      expect(all('.charge').count).to eq successful_charges.count
+    end
+  end
+
+
 end
