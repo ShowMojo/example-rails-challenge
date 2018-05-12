60cf23055e427a8bfeb019ce4f277ad951b0efe1
Victor Pukman <victor.pukman@gmail.com>
2018-05-13 00:17:41 +0800

    charge scopes added

--- source/spec/models/charge_spec.rb
+++ source/spec/models/charge_spec.rb
@@ -29,5 +29,26 @@ RSpec.describe Charge, type: :model do
     it { expect(charge).to belong_to(:customer) }
   end
 
+  describe "scope" do
+    let!(:successful_charges) { create_list :charge, 5, :successful }
+    let!(:failed_charges) { create_list :charge, 3, :failed }
+    let!(:disputed_charges) { create_list :charge, 10, :disputed }
+    describe "#successful" do
+      it "returns successful charges" do
+        expect(Charge.successful).to match_array successful_charges
+      end
+    end
+    describe "#failed" do
+      it "returns failed charges" do
+        expect(Charge.failed).to match_array failed_charges
+      end
+    end
+    describe "#disputed" do
+      it "returns disputed charges" do
+        expect(Charge.disputed).to match_array disputed_charges
+      end
+    end
+  end
+
 
 end
