90ef6799d7458aa1224870fcf5cccbac271f01ef
Victor Pukman <victor.pukman@gmail.com>
2018-05-13 01:33:37 +0800

    seeds added

--- source/db/seeds.rb
+++ source/db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+c1 = Customer.create!({first_name: "Johny", last_name: "Flow"})
+c2 = Customer.create!({first_name: "Raj", last_name: "Jamnis"})
+c3 = Customer.create!({first_name: "Andrew", last_name: "Chung"})
+c4 = Customer.create!({first_name: "Mike", last_name: "Smith"})
+
+
+5.times { Charge.successful.create!(amount: rand(50..150), currency: "usd", customer: c1) }
+3.times { Charge.successful.create!(amount: rand(50..150), currency: "usd", customer: c2) }
+1.times { Charge.successful.create!(amount: rand(50..150), currency: "usd", customer: c3) }
+1.times { Charge.successful.create!(amount: rand(50..150), currency: "usd", customer: c4) }
+
+3.times { Charge.failed.create!(amount: rand(50..150), currency: "usd", customer: c3) }
+2.times { Charge.failed.create!(amount: rand(50..150), currency: "usd", customer: c4) }
+
+3.times { Charge.disputed.create!(amount: rand(50..150), currency: "usd", customer: c1) }
+2.times { Charge.disputed.create!(amount: rand(50..150), currency: "usd", customer: c2) }
