bc2f4dd8632b0e282d6406dfd00952cd8fc7cd5e
Fernando Serapio <fhernandosc@gmail.com>
2018-12-02 14:44:19 -0600

    add seeds

--- source/db/seeds.rb
+++ source/db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Successful transactions
+johny = Customer.create first_name: "Johny", last_name: "Flow"
+raj = Customer.create first_name: "Raj", last_name: "Jamis"
+andrew = Customer.create first_name: "Andrew", last_name: "Chung"
+mike = Customer.create first_name: "Mike", last_name: "Smith"
+
+5.times do
+  CreditCardCharge.create customer: johny, amount: rand(1..1000).to_f, paid: true, currency: :usd
+end
+3.times do
+  CreditCardCharge.create customer: raj, amount: rand(1..1000).to_f, paid: true, currency: :usd
+end
+CreditCardCharge.create customer: andrew, amount: rand(1..1000).to_f, paid: true, currency: :usd
+CreditCardCharge.create customer: mike, amount: rand(1..1000).to_f, paid: true, currency: :usd
+
+# Failed transactions
+3.times do
+  CreditCardCharge.create customer: andrew, amount: rand(1..1000).to_f, currency: :usd
+end
+2.times do
+  CreditCardCharge.create customer: mike, amount: rand(1..1000).to_f, currency: :usd
+end
+
+# Disputed transactions
+3.times do
+  CreditCardCharge.create customer: johny, amount: rand(1..1000).to_f, refunded: true, currency: :usd
+end
+2.times do
+  CreditCardCharge.create customer: raj, amount: rand(1..1000).to_f, refunded: true, currency: :usd
+end
