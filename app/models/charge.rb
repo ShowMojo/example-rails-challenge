2ac6d0e0437274c3f0fcf7b2a8aee3d6709917b4
Gustavo <gustavo@MacBook-Pro-de-Mac.local>
2018-12-07 01:43:51 -0200

    Charges listing feature

--- app/models/charge.rb
+++ app/models/charge.rb
@@ -1,3 +1,7 @@
 class Charge < ActiveRecord::Base
   belongs_to :customer
+
+  scope :failed, -> { where('refunded is ? AND paid is ?', false, false) }
+  scope :successful, -> { where('refunded is ? AND paid is ?', false, true) }
+  scope :disputed, -> { where('refunded is ? AND paid is ?', true, true) }
 end
