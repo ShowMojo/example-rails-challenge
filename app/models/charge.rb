28023e0f0301ad9ee1a39393fcf4c6a27b35dcdc
Gustavo <gustavo@MacBook-Pro-de-Mac.local>
2018-12-07 02:12:40 -0200

    Fixing N+1 problem

--- app/models/charge.rb
+++ app/models/charge.rb
@@ -1,7 +1,7 @@
 class Charge < ActiveRecord::Base
   belongs_to :customer
 
-  scope :failed, -> { where('refunded is ? AND paid is ?', false, false) }
-  scope :successful, -> { where('refunded is ? AND paid is ?', false, true) }
-  scope :disputed, -> { where('refunded is ? AND paid is ?', true, true) }
+  scope :failed, -> { where('(refunded is ? AND paid is ?) OR (refunded is ? AND paid is ?)', false, false, true, true).includes(:customer) }
+  scope :successful, -> { where('refunded is ? AND paid is ?', false, true).includes(:customer) }
+  scope :disputed, -> { where('refunded is ? AND paid is ?', true, true).includes(:customer) }
 end
