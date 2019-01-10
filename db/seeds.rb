4dfe4de18cd0b5b8fe709e643c24df3e5bd9b14e
Diego Toral <diegotoral@gmail.com>
2019-01-10 09:05:33 -0300

    Add basic seeds with customers and charges

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts '*** Seeding customers ***'
+
+customers_attrs = [
+  { first_name: 'Johny', last_name: 'Flow' },
+  { first_name: 'Raj', last_name: 'Jamnis' },
+  { first_name: 'Andrew', last_name: 'Chung' },
+  { first_name: 'Mike', last_name: 'Smith' }
+]
+
+customers = customers_attrs.map { |attrs| Customer.find_or_create_by!(attrs) }
+
+puts '*** Seeding charges ***'
+def assign_random_charges(customer, count, status)
+  attrs = Array.new(count) { { amount: rand(100..10_000), status: status } }
+  customer.charges.create!(attrs)
+end
+assign_random_charges(customers[0], 5, :successful)
+assign_random_charges(customers[0], 3, :disputed)
+
+assign_random_charges(customers[1], 3, :successful)
+assign_random_charges(customers[1], 2, :disputed)
+
+assign_random_charges(customers[2], 1, :successful)
+assign_random_charges(customers[2], 3, :failed)
+
+assign_random_charges(customers[3], 1, :successful)
+assign_random_charges(customers[3], 2, :failed)
