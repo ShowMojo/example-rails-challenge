b7765b3f7ca418c5ec3865d14c919f66a35a7649
Alex Leasenco <rubydeve@gmail.com>
2019-01-23 14:45:07 +0200

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+customer_1 = Customer.create(first_name: 'Johny', last_name: 'Flow')
+customer_2 = Customer.create(first_name: 'Raj', last_name: 'Jamnis')
+customer_3 = Customer.create(first_name: 'Andrew', last_name: 'Chung')
+customer_4 = Customer.create(first_name: 'Mike', last_name: 'Smith')
+
+# Seed 20 transactions (charges) into the system:
+# 10 Should be successful transactions:
+# - 5 Should be linked to Customer 1
+# - 3 Should be linked to Customer 2
+# - 1 Should be linked to Customer 3
+# - 1 Should be linked to Customer 4
+5.times { customer_1.charges.create(paid: true, amount: rand(100), currency: 'usd', refunded: false) }
+3.times { customer_2.charges.create(paid: true, amount: rand(100), currency: 'usd', refunded: false) }
+customer_3.charges.create(paid: true, amount: rand(100), currency: 'usd', refunded: false)
+customer_4.charges.create(paid: true, amount: rand(100), currency: 'usd', refunded: false)
+
+# 5 Should be transactions that failed:
+# - 3 Should be linked to Customer 3
+# - 2 Should be linked to Customer 4
+3.times { customer_3.charges.create(paid: false, amount: rand(100), currency: 'usd', refunded: false) }
+2.times { customer_4.charges.create(paid: false, amount: rand(100), currency: 'usd', refunded: false) }
+
+# 5 should be disputed:
+# - 3 should be linked to Customer 1
+# - 2 should be linked to customer 2
+3.times { customer_1.charges.create(paid: true, amount: rand(100), currency: 'usd', refunded: true) }
+2.times { customer_2.charges.create(paid: true, amount: rand(100), currency: 'usd', refunded: true) }
