3e895a935ef9a86cf9357111d5e37309cb2a618a
Eligijus Petkus <petkus.eligijus@gmail.com>
2019-02-06 18:14:23 +0000

    Adds relationship, scopes and updates seeds file

--- app/models/charge.rb
+++ app/models/charge.rb
@@ -1,2 +1,8 @@
 class Charge < ActiveRecord::Base
+  belongs_to :customer
+
+  # Scopes
+  scope :successfull, -> { where(paid: true, refunded: false) }
+  scope :failed, -> { where(paid: false) }
+  scope :disputed, -> { where(paid: true, refunded: true) }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+puts "*** Seeding Customers ***"
+customer1 = Customer.create!(first_name: 'Johny', last_name: 'Flow')
+customer2 = Customer.create!(first_name: 'Ra', last_name: 'Jamnis')
+customer3 = Customer.create!(first_name: 'Andre', last_name: 'Chung')
+customer4 = Customer.create!(first_name: 'Mik', last_name: 'Smith')
+
+puts "*** Seeding Charges ***"
+
+def create_charge(status, customer, count)
+  puts "\t Creating #{count} #{status} Charges for Customer #{customer.id}"
+
+  count.times do
+    Charge.send(status).create(
+      customer_id: customer.id,
+      amount: rand(100..99999),
+      currency: 'usd',
+      created_at: rand(2.years).seconds.ago
+    )
+  end
+end
+
+# Succeffull charges
+create_charge(:successfull, customer1, 5)
+create_charge(:successfull, customer2, 3)
+create_charge(:successfull, customer3, 1)
+create_charge(:successfull, customer4, 1)
+
+# Failed charges
+create_charge(:failed, customer3, 3)
+create_charge(:failed, customer4, 2)
+
+# Disputed charges
+create_charge(:disputed, customer1, 3)
+create_charge(:disputed, customer2, 2)
+
+puts "*** Finished Seeding ***"
