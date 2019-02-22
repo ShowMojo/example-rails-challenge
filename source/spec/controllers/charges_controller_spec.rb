26bf4829bdc8df813955b2c681126644c67e3877
Efren Fuentes <efrenfuentes@gmail.com>
2019-02-22 19:53:38 -0400

    Tests for finish the project

--- source/spec/controllers/charges_controller_spec.rb
+++ source/spec/controllers/charges_controller_spec.rb
@@ -1,12 +1,3 @@
 require 'rails_helper'
 
-RSpec.describe ChargesController, type: :controller do
-
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-end
+RSpec.describe ChargesController, type: :controller 
