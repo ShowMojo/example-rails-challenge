c11e6fdb2c72aacff468e9228f50bbf3127ac817
J Guthrie <jamie.guthrie@gmail.com>
2019-03-29 14:11:45 +0000

    Add seeds

--- source/db/seeds.rb
+++ source/db/seeds.rb
@@ -5,3 +5,59 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#####
+##
+## Add Customers
+##
+#####
+
+custs = [
+  "Jonny Flow",
+  "Raj Jamnis",
+  "Andrew Chung",
+  "Mike Smith"
+]
+
+custs.each do |cust|
+  Customer.create(first_name: cust.split(" ")[0], last_name: cust.split(" ")[1])
+end
+
+########
+
+#####
+##
+##Successful transactions
+##
+
+cust = Customer.find(1)
+10.times do |i|
+  cust = Customer.find(2) if i == 5
+  cust = Customer.find(3) if i == 8
+  cust = Customer.find(4) if i == 9
+
+  CreditCardCharge.create(paid: true, amount: rand(5000000), customer: cust)
+end
+
+#####
+##
+##Failed transactions
+##
+
+cust = Customer.find(3)
+5.times do |i|
+  cust = Customer.find(4) if i == 3
+
+  CreditCardCharge.create(paid: false, amount: rand(5000000), customer: cust)
+end
+
+#####
+##
+##Disputed transactions
+##
+cust = Customer.find(1)
+5.times do |i|
+  cust = Customer.find(2) if i == 3
+
+  CreditCardCharge.create(paid: true, amount: rand(5000000), refunded: true, customer: cust)
+end
