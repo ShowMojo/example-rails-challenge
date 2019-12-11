d5648e179884f4d296159dde005afd4683c2caec
Max Popov <max.popov@gmail.com>
2019-12-12 00:32:09 +0300

    factory config

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
-FactoryBot.find_definitions
-
 c1 = Customer.find_or_create_by!(
   first_name: "Johny",
   last_name:  "Flow"
@@ -49,7 +47,7 @@ if Charge.count.zero?
   2.times do
     FactoryBot.create(:charge, paid: true, refunded: true, customer: c2)
   end
-  
+
   p "Charges created"
 end
 
