8e5f6bf6a1eeaa1d832209c8a6b9ca9c64dc4d30
Max Popov <max.popov@gmail.com>
2019-12-11 23:38:51 +0300

    Charges view finished

--- app/models/charge.rb
+++ app/models/charge.rb
@@ -4,4 +4,8 @@ class Charge < ApplicationRecord
   enum currency: [:usd, :euro]
 
   validates_presence_of :created, :amount, :currency, :customer_id
+
+  scope :failed,     -> { where("paid = ?", false).order(created: "DESC") }
+  scope :disputed,   -> { where("paid = ?", true).where("refunded = ?", true).order(created: "DESC") }
+  scope :successful, -> { where("paid = ?", true).where("refunded = ?", false).order(created: "DESC") }
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,8 @@ class Customer < ApplicationRecord
   validates_presence_of :first_name, :last_name
 
   has_many :charges
+
+  def full_name
+    first_name + " " + last_name
+  end
 end
