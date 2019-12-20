36d45daae1b486ebfbc7cde9d371c3053b4a0fa3
Dan <dansification@gmail.com>
2019-12-19 23:31:28 -0400

    Add scaffolds for Customer and Charge

---
+++ source/db/migrate/20191220032301_create_charges.rb
@@ -0,0 +1,12 @@
+class CreateCharges < ActiveRecord::Migration
+  def change
+    create_table :charges do |t|
+      t.integer :created, null: false
+      t.boolean :paid, null: false, default: false
+      t.integer :amount, null: false, default: 0
+      t.string :currency, null: false
+      t.boolean :refunded, null: false, default: false
+      t.references :customer, index: true, foreign_key: true
+    end
+  end
+end
