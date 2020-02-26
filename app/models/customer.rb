5b52d94519ba7fe87820a6bcfcf3b5869ec34f27
Shiva Bhusal <hotline.shiva@gmail.com>
2020-02-26 18:25:29 +0545

    Generates Customer model and creates seed

---
+++ app/models/customer.rb
@@ -0,0 +1,12 @@
+# == Schema Information
+#
+# Table name: customers
+#
+#  id         :integer          not null, primary key
+#  first_name :string
+#  last_name  :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Customer < ApplicationRecord
+end
