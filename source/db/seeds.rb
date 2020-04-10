feeaaa8c5aed5345c65f62a961c60857f7202094
Artem Kulakov <artem.kulakov@gmail.com>
2020-04-10 23:05:33 +0300

    Seed Charge

--- source/db/seeds.rb
+++ source/db/seeds.rb
@@ -1,18 +1,98 @@
-Customer.create(
+Customer.delete_all
+
+customer_1 = Customer.create(
   first_name: 'Johny',
   last_name: 'Flow'
 )
-Customer.create(
+
+customer_2 = Customer.create(
   first_name: 'Raj',
   last_name: 'Jamnis'
 )
 
-Customer.create(
+customer_3 = Customer.create(
   first_name: 'Andrew',
   last_name: 'Chung'
 )
 
-Customer.create(
+customer_4 = Customer.create(
   first_name: 'Mike',
   last_name: 'Smith'
 )
+
+
+5.times do
+  Charge.create(
+    paid: true,
+    amount: 10000,
+    currency: 'usd',
+    refunded: false,
+    customer: customer_1
+  )
+end
+
+3.times do
+  Charge.create(
+    paid: true,
+    amount: 10000,
+    currency: 'usd',
+    refunded: false,
+    customer: customer_2
+  )
+end
+
+Charge.create(
+  paid: true,
+  amount: 10000,
+  currency: 'usd',
+  refunded: false,
+  customer: customer_3
+)
+
+Charge.create(
+  paid: true,
+  amount: 10000,
+  currency: 'usd',
+  refunded: false,
+  customer: customer_4
+)
+
+3.times do
+  Charge.create(
+    paid: false,
+    amount: 10000,
+    currency: 'usd',
+    refunded: false,
+    customer: customer_3
+  )
+end
+
+2.times do
+  Charge.create(
+    paid: false,
+    amount: 10000,
+    currency: 'usd',
+    refunded: false,
+    customer: customer_4
+  )
+end
+
+3.times do
+  Charge.create(
+    paid: true,
+    amount: 10000,
+    currency: 'usd',
+    refunded: true,
+    customer: customer_1
+  )
+end
+
+2.times do
+  Charge.create(
+    paid: true,
+    amount: 10000,
+    currency: 'usd',
+    refunded: true,
+    customer: customer_2
+  )
+end
