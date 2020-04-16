8e8144900d213badacd68fb1a18114efba655e14
lijin1991-toptal <li.jin@toptal.com>
2020-04-16 17:24:46 +0300

    finished test task

--- source/test/controllers/charges_controller_test.rb
+++ source/test/controllers/charges_controller_test.rb
@@ -5,12 +5,6 @@ class ChargesControllerTest < ActionController::TestCase
     @charge = charges(:one)
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:charges)
-  end
-
   test "should get new" do
     get :new
     assert_response :success
