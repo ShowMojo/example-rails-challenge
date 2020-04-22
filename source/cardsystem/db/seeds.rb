4a7d5a9779741e30192ba67ad0bc068b14f411ee
Krisztián Ádám <adamkrisz@gmail.com>
2020-04-22 20:40:03 +0200

    create models + seeds

--- source/cardsystem/db/seeds.rb
+++ source/cardsystem/db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +8,36 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+customers = Customer.create(
+  [
+    {
+
+      first_name: 'Johny',
+      last_name: 'Flow'
+    },
+    {
+
+      first_name: 'Raj',
+      last_name: 'Jamnis'
+    },
+    {
+      first_name: 'Andrew',
+      last_name: 'Chung'
+    },
+    {
+      first_name: 'Mike',
+      last_name: 'Smith'
+    }
+  ]
+)
+
+5.times { Charge.create(customer: customers[0], paid: true, currency: 'usd', amount: 1, refunded: false) }
+3.times { Charge.create(customer: customers[1], paid: true, currency: 'usd', amount: 1, refunded: false) }
+1.times { Charge.create(customer: customers[2], paid: true, currency: 'usd', amount: 1, refunded: false) }
+1.times { Charge.create(customer: customers[3], paid: true, currency: 'usd', amount: 1, refunded: false) }
+
+3.times { Charge.create(customer: customers[2], paid: false, currency: 'usd', amount: 1, refunded: false) }
+2.times { Charge.create(customer: customers[3], paid: false, currency: 'usd', amount: 1, refunded: false) }
+
+3.times { Charge.create(customer: customers[0], paid: true, currency: 'usd', amount: 1, refunded: true) }
+2.times { Charge.create(customer: customers[1], paid: true, currency: 'usd', amount: 1, refunded: true) }
