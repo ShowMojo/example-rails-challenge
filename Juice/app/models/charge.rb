e89c743add351baffa9809dbadb5b60694453045
Jeff Lai <winfield301@gmail.com>
2020-05-01 16:41:22 -0700

    Add customer & charge models

---
+++ Juice/app/models/charge.rb
@@ -0,0 +1,15 @@
+class Charge < ApplicationRecord
+  belongs_to :customer
+
+  before_validation :set_created
+
+  scope :successful, -> { where(paid: true, refunded: false) }
+  scope :failed, -> { where(paid: false, refunded: false) }
+  scope :disputed, -> { where(paid: true, refunded: true) }
+
+  private
+
+  def set_created
+    self.created = Time.current.to_i
+  end
+end
