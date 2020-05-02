f7698588aa9fbe75b151f1a65749e219d82f8509
Jeff Lai <winfield301@gmail.com>
2020-05-01 17:02:53 -0700

    Add specs to verify lists
    Verify that there are three lists on the screen.
    One for successful charges.
    One for failed charges.
    One for the disputed charges.

---
+++ Juice/spec/views/charges/index.html.erb_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe "charges/index", type: :view do
+  before(:each) do
+    assign(:successful_charges, Charge.successful)
+    assign(:failed_charges, Charge.failed)
+    assign(:disputed_charges, Charge.failed.or(Charge.disputed))
+  end
+
+  it "renders 3 lists of charges" do
+    render
+    assert_select 'ul', count: 3
+  end
+
+  it "renders 1 list of successful charges" do
+    render
+    assert_select 'ul.successful-list', count: 1
+  end
+
+  it "renders 1 list of disputed charges" do
+    render
+    assert_select 'ul.disputed-list', count: 1
+  end
+
+  it "renders 1 list of failed charges" do
+    render
+    assert_select 'ul.failed-list', count: 1
+  end
+end
