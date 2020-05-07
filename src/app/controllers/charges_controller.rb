c17b1fd2f3157911aa4ff0b674c802965e902744
Vadim Karnopelev <vadim7j7@gmail.com>
2020-05-07 21:19:42 +0800

    Render lists.

--- src/app/controllers/charges_controller.rb
+++ src/app/controllers/charges_controller.rb
@@ -1,10 +1,15 @@
 class ChargesController < ApplicationController
   def index
+    @charges = Charge
+
     respond_to do |format|
-      format.html
-      format.json do
-        @charges = Charge.all
+      format.html do
+        @charges = @charges.includes(:customer)
+        @list_failed = @charges.where(state: 1)
+        @list_disputed = @charges.where(state: 2)
+        @list_success = @charges.where(state: 0)
       end
+      format.json
     end
   end
 end
--- src/app/models/customer.rb
+++ src/app/models/customer.rb
@@ -2,4 +2,8 @@ class Customer < ActiveRecord::Base
   has_many :charges, dependent: :destroy
 
   validates :first_name, :last_name, presence: true
+
+  def name
+    [first_name, last_name].join(' ')
+  end
 end
