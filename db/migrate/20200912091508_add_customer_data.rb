00a3b153c1d5cb758e1fe7454ba79f0a03a431c0
taras-sarmat <taras_sarmat@ukr.net>
2020-09-12 12:18:41 +0300

    Added initialize data for Customers

---
+++ db/migrate/20200912091508_add_customer_data.rb
@@ -0,0 +1,8 @@
+class AddCustomerData < ActiveRecord::Migration
+  def change
+  	Customer.create(first_name: 'Johny', last_name: 'Flow')
+  	Customer.create(first_name: 'Raj', last_name: 'Jamnis')
+  	Customer.create(first_name: 'Andrew', last_name: 'Chung')
+  	Customer.create(first_name: 'Mike', last_name: 'Smith')
+  end
+end
