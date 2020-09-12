fa85b09fc0dae95ab11c58a4c358b7ff32f52732
taras-sarmat <taras_sarmat@ukr.net>
2020-09-12 12:52:31 +0300

    Added Representation of Disputed Charges

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -5,6 +5,7 @@ class ChargesController < ApplicationController
   # GET /charges.json
   def index
     @failed_charges = Charge.where(paid: false)
+    @disputed_charges = Charge.where(refunded: false)
   end
 
   # GET /charges/1
