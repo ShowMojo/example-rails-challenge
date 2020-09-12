8032bfeef40f47c607f627e29d0eb372738a0c78
taras-sarmat <taras_sarmat@ukr.net>
2020-09-12 12:13:27 +0300

    Added scaffolded Charge entity

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200912090019) do
+ActiveRecord::Schema.define(version: 20200912091046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "charges", force: :cascade do |t|
+    t.boolean  "paid"
+    t.integer  "amount"
+    t.string   "currency"
+    t.boolean  "refunded"
+    t.integer  "customer_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "charges", ["customer_id"], name: "index_charges_on_customer_id", using: :btree
 
   create_table "customers", force: :cascade do |t|
     t.string   "first_name"
@@ -23,4 +35,5 @@ ActiveRecord::Schema.define(version: 20200912090019) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "charges", "customers"
 end
