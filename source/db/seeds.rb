b7d553ff74d6fdfb6faa2f0e76ed61269e9fe087
Martin Silenus <sileenus@gmail.com>
2020-09-17 01:41:03 +0300

    add seeds

--- source/db/seeds.rb
+++ source/db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Customer.delete_all
+CreditCardCharge.delete_all
+
+customers_data = [
+  { name: "Johny Flow", successful: 5, failed: 0, disputed: 3 },
+  { name: "Raj Jamnis", successful: 3, failed: 0, disputed: 2 },
+  { name: "Andrew Chung", successful: 1, failed: 3, disputed: 0 },
+  { name: "Mike Smith", successful: 1, failed: 2, disputed: 0 },
+]
+
+customers_data.each do |data|
+  first_name, last_name = data[:name].split(' ')
+  customer = Customer.create(first_name: first_name, last_name: last_name)
+
+  [:successful, :failed, :disputed].each do |charge_type|
+    charge_type_attrs = {
+      successful: { paid: true, currency: 'usd', refunded: false, customer: customer},
+      failed: { paid: false, currency: 'usd', refunded: false, customer: customer},
+      disputed: { paid: true, currency: 'usd', refunded: true, customer: customer}
+    }
+    1000.step(by: 1000, to: data[charge_type] * 1000) do |amount|
+      CreditCardCharge.create(charge_type_attrs[charge_type].merge(amount: amount))
+    end
+  end
+end
