90a3feb6cf84476e8888eb69d944d91396fac8d8
Alex Kochurov <alex.kochurov@gmail.com>
2021-04-20 23:20:38 +0300

    complete seeds

--- source/app/models/charge.rb
+++ source/app/models/charge.rb
@@ -1,3 +1,11 @@
 class Charge < ActiveRecord::Base
   belongs_to :customer
+
+  def self.new_random_payment(attributes)
+    new(attributes.merge(amount: rand(1000_00) + 1, currency: %w(usd eur rub chf gbp).sample))
+  end
+
+  def refund!
+    update!(refunded: true)
+  end
 end
--- source/db/seeds.rb
+++ source/db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Seed the customers
+customers =
+  [%w(Johny Flow), %w(Raj Jamnis), %w(Andrew Chung), %w(Mike Smith)].map do |name, sirname|
+    Customer.find_or_create_by(first_name: name, last_name: sirname)
+  end
+
+# Successful transactions
+[5, 3, 1, 1].each_with_index do |transactions_number, customer_id|
+  transactions_number.times do
+    charge = Charge.new_random_payment(paid: true, customer: customers[customer_id])
+    charge.save!
+  end
+end
+
+# Failed transactions
+[0, 0, 3, 2].each_with_index do |transactions_number, customer_id|
+  transactions_number.times do
+    charge = Charge.new_random_payment(paid: false, customer: customers[customer_id])
+    charge.save!
+  end
+end
+
+# Disputed transactions - as i understood it's paid, but refunded charges
+[3, 2].each_with_index do |transactions_number, customer_id|
+  transactions_number.times do
+    charge = Charge.new_random_payment(paid: true, customer: customers[customer_id])
+    charge.save!
+    charge.refund!
+  end
+end
