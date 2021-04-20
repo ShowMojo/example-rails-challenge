b360126e5420260f2b9e78c179b614249980249e
abdelp <juniorperezpy@gmail.com>
2021-04-20 00:22:48 -0400

    Add default value for Transactions

--- source/exampleApp/db/migrate/20210420033157_create_transactions.rb
+++ source/exampleApp/db/migrate/20210420033157_create_transactions.rb
@@ -1,14 +1,23 @@
 class CreateTransactions < ActiveRecord::Migration
   def change
     create_table :transactions do |t|
-      t.integer :created, null: false, default: "extract('epoch' from CURRENT_TIMESTAMP)::bigint"
+      t.integer :created, null: false
       t.boolean :paid, null: false, default: false
       t.decimal :amount
       t.string :currency, null: false
       t.boolean :refunded, null: false, default: false
       t.references :customer, index: true, null: false
 
+      
       t.timestamps
+    end
+    reversible do |dir|
+      dir.up do
+        execute <<-SQL
+          ALTER TABLE transactions
+            ALTER COLUMN created SET DEFAULT date_part('epoch', current_timestamp);
+        SQL
+      end
     end
   end
 end
