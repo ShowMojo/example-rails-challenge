4ad1cc08dd91d1536f8da4f14861c553e644d1de
jacky <jackyinvn@gmail.com>
2021-04-21 22:57:09 +0700

    Add seed

--- source/card_charge/db/seeds.rb
+++ source/card_charge/db/seeds.rb
@@ -1,7 +1,62 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# customers ##
+customers = [
+  {
+    first_name: 'Johny',
+    last_name: 'Flow'
+  },
+  {
+    first_name: 'Raj',
+    last_name: 'Jamnis'
+  },
+  {
+    first_name: 'Andrew',
+    last_name: 'Chung'
+  },
+  {
+    first_name: 'Mike',
+    last_name: 'Smith'
+  }
+]
+
+customers = customers.map do |cus|
+  Customer.create(cus)
+end
+
+# transactions ##
+# success
+success_charge = {
+  paid: true,
+  amount: rand(1000),
+  currency: 'usd',
+  refunded: false,
+  created: Time.current.to_i
+}
+
+5.times do
+  customers[0].charges.create(success_charge)
+end
+
+3.times do
+  customers[1].charges.create(success_charge)
+end
+
+customers[2].charges.create(success_charge)
+customers[3].charges.create(success_charge)
+
+# failed
+3.times do
+  customers[2].charges.create(success_charge.merge!(paid: false))
+end
+
+2.times do
+  customers[3].charges.create(success_charge.merge!(paid: false))
+end
+
+# disputed
+3.times do
+  customers[0].charges.create(success_charge.merge!(refunded: true))
+end
+
+2.times do
+  customers[1].charges.create(success_charge.merge!(refunded: true))
+end
