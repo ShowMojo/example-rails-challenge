a32f18b5a86b2d032a42e4cd38847bd4e7dc7d7d
Giorgos Avramidis <avramidg@gmail.com>
2021-05-31 16:22:20 +0300

    customer has_many charges

--- slowmojo/spec/models/customer_spec.rb
+++ slowmojo/spec/models/customer_spec.rb
@@ -5,4 +5,5 @@ RSpec.describe Customer, type: :model do
 
   it { is_expected.to validate_presence_of(:first_name) }
   it { is_expected.to validate_presence_of(:last_name) }
+  it { is_expected.to have_many(:charges).dependent(:destroy) }
 end
